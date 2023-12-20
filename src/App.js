104e4128de6e5082d265a51cd6e55cf937a1721d
唐杰华 <2640427168@qq.com>
2023-12-20 16:46:39 +0800

    建立方块

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useEffect } from 'react';
+import { createScene } from './component_three/scene';
+import { createCamera } from './component_three/camera';
+import { createRenderer } from './systems_three/renderer';
+import { createCube } from './component_three/cube';
 
 function App() {
+  const sceneRef = useRef(null);
+  const rendererRef = useRef(null);
+  useEffect(() => {
+    const scene = createScene();
+    const camera = createCamera();
+    camera.position.z = 5;
+
+    const renderer = createRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    rendererRef.current = renderer;
+    const cube = createCube;
+    scene.add(cube);
+
+    sceneRef.current.appendChild(renderer.domElement);
+
+    // const animate = () => {
+    //   requestAnimationFrame(animate);
+
+    //   cube.rotation.x += 0.01;
+    //   cube.rotation.y += 0.01;
+
+    //   renderer.render(scene, camera);
+    // };
+
+    // animate();
+
+    return () => {
+      sceneRef.current.removeChild(renderer.domElement);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={sceneRef}>
+
     </div>
   );
 }
